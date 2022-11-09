b0bbfb02ec6cd683c2f0b3fe384e50a90ee5a42c
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-09 22:09:55 +0530

    Create RelationalOperators.java

---
+++ Days/Day003/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int p = 45;
+        int q = 29;
+        System.out.println(p==q); //false
+        System.out.println(p!=q); //true
+        System.out.println(p<=q); //false
+        System.out.println(p>=q); //true
+        System.out.println(p<q); //false
+        System.out.println(p>q); //true
+    }
+}
