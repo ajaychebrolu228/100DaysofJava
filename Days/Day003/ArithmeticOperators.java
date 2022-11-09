1daddba4bfd7b53ccdca71782dfb9138ff046797
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-09 22:01:37 +0530

    Create ArithmeticOperators.java

---
+++ Days/Day003/ArithmeticOperators.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int p = 40;
+        int q = 15;
+        System.out.println(p+q); //55
+        System.out.println(p-q); //25
+        System.out.println(p/q); //2
+        System.out.println(p%q); //10
+    }
+}
