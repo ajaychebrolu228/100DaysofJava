7780ef13da714270f9addfc1e7a40e93f296417e
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-10 20:40:51 +0530

    Create WhileLoop.java

---
+++ Days/Day004/WhileLoop.java
@@ -0,0 +1,14 @@
+// This program print natural numbers upto given input
+import java.util.Scanner;
+public class WhileLoop {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+        int i = 1;
+        while(i<=n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
