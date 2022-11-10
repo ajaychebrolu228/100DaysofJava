8178d1b16f4fb778615fd7529e1ed1089d791c2c
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-10 22:01:58 +0530

    Create SumofDigits.java

---
+++ Days/Day004/SumofDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumofDigits {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int sum = 0;
+        int n = sc.nextInt();
+        while(n > 0) {
+            int i = n%10;
+            n /= 10;
+            sum += i;
+        }
+        System.out.println("Sum of Digits = "+sum);
+    }
+}
