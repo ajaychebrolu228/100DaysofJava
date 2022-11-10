e87a53defd205ab7807a287146e3c003bb49f389
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-10 22:10:11 +0530

    Update SumofDigits.java

--- Days/Day004/SumofDigits.java
+++ Days/Day004/SumofDigits.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class SumofDigits {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
+        System.out.print("Enter the number: ");
         int sum = 0;
         int n = sc.nextInt();
         while(n > 0) {
