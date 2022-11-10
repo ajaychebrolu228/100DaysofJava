3bab598ad741c7273bac4cb138f9bd141dc77945
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-10 21:47:42 +0530

    Create Continue.java

---
+++ Days/Day004/Continue.java
@@ -0,0 +1,10 @@
+public class Continue {
+    public static void main(String args[]) {
+        for(int i = 1; i <=20; i++) {
+            if(i==5||i==10||i==15) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
