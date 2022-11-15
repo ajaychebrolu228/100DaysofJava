16450f23a070e86f36ea3b9dfe15bc056aaf58eb
Ajay Babu Chebrolu <115444382+ajay-chebrolu@users.noreply.github.com>
2022-11-15 21:52:17 +0530

    Create SampleArray.java

---
+++ Days/Day009/SampleArray.java
@@ -0,0 +1,9 @@
+ public class SampleArray {
+ 	public static void main(String args[]) {
+ 		int sample[] = new int[10];
+ 		for(int i = 0; i < 10; i++)
+ 			sample[i] = i;
+ 		for(int i = 0; i < 10; i++)
+ 			System.out.println(i);
+ 	}
+ }
